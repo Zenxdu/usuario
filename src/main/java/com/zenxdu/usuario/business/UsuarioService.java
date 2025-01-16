da09f76d2371498e5db7d4e6185c5b60a86402e9
Eduardo <eduardo.coltro@hotmail.com>
2025-01-16 15:36:38 -0300

    Inclusão de metodo put de usuario

--- src/main/java/com/zenxdu/usuario/business/UsuarioService.java
+++ src/main/java/com/zenxdu/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.zenxdu.usuario.infrastructure.entity.Usuario;
 import com.zenxdu.usuario.infrastructure.exceptions.ConflictException;
 import com.zenxdu.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.zenxdu.usuario.infrastructure.repository.UsuarioRepository;
+import com.zenxdu.usuario.infrastructure.security.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -49,5 +51,21 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        //Criptografia de senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado"));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+
     }
 }
--- src/main/java/com/zenxdu/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/zenxdu/usuario/business/converter/UsuarioConverter.java
@@ -107,4 +107,15 @@ public class UsuarioConverter {
                 .ddd(telefoneDTO.getDdd())
                 .build();
     }
+
+    public Usuario updateUsuario(UsuarioDTO usuario, Usuario entity){
+        return Usuario.builder()
+                .nome(usuario.getNome() != null ? usuario.getNome() : entity.getNome())
+                .id(entity.getId())
+                .senha(usuario.getSenha() != null ? usuario.getSenha() : entity.getSenha())
+                .email(usuario.getEmail() != null ? usuario.getEmail() : entity.getEmail())
+                .enderecos(entity.getEnderecos())
+                .telefones(entity.getTelefones())
+                .build();
+    }
 }
